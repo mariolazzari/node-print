1a5e302893a36f6d9d9e32bd3cf5ede1ac5a089f
Mario Lazzari <mario.lazzari@italcoeng.com>
2019-11-28 17:10:27 +0100

    Updated options

--- puppeteer.js
+++ puppeteer.js
@@ -5,14 +5,17 @@ const path = require("path");
 const LABEL = path.join(__dirname, "data", "label.jpg");
 const FILE = path.join(__dirname, "data", "file.pdf");
 const SITE = path.join(__dirname, "data", "site.pdf");
+// puppeteer options
+const viewPort = { width: 1440, height: 900, deviceScaleFactor: 2 };
+const goto = { waitUntil: "networkidle2" };
 
 // save url as PDF
 const printSite = async (url = "https://italcoeng.com") => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
-    await page.goto(url, { waitUntil: "networkidle2" });
+    await page.setViewport(viewPort);
+    await page.goto(url, goto);
     await page.pdf({
       path: SITE,
       pageRanges: "1",
@@ -30,8 +33,8 @@ const printFile = async (file = LABEL) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
-    await page.goto(`file://${file}`, { waitUntil: "networkidle2" });
+    await page.setViewport(viewPort);
+    await page.goto(`file://${file}`, goto);
     await page.pdf({
       path: FILE,
       pageRanges: "1",
