0622592d55792b8ea8d5d6057ce94f06346549ff
Mario Lazzari <mario.lazzari@italcoeng.com>
2019-11-28 08:58:35 +0100

    Added pdf-to-printer demo

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("NodeJS printing tools demos.");
---
+++ printPDF.js
@@ -0,0 +1,26 @@
+// https://www.npmjs.com/package/pdf-to-printer
+const printer = require("pdf-to-printer");
+const TEST_FILE = "test.pdf";
+
+// show printers list
+printer
+  .list()
+  .then(printers => console.log("Printers found:", printers))
+  .catch(console.error);
+
+// printer config
+const options = {
+  //printer: "Multifunzione"
+  printer: "PDFCreator" // default one, if no printer option used
+};
+
+// send pdf directly to printer
+const printPDF = async (pdf, options = {}) => {
+  try {
+    await printer.print(pdf, options);
+    console.log(`${pdf} printed succesfully.`);
+  } catch (ex) {
+    console.error(`Error while sending ${pdf} to printer.`, ex);
+  }
+};
+printPDF(TEST_FILE, options);
